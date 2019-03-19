38f4c85def14354cb794d8f1de92e052a38638ba
konryotada <konryotada@gmail.com>
2019-03-19 10:47:42 +0900

    再修正

--- Reveisible/Select_locate.py
+++ Reveisible/Select_locate.py
@@ -11,10 +11,13 @@ class Select_locate:
             self.select_locate(field)
         self.input_num[0] = int(input_num[0])-1
         self.input_num[1] = int(input_num[1])-1
+        if self.input_num[0] >= len(field[0]) or self.input_num[1] >= len(field[0]) or self.input_num[0] < 0 or self.input_num[1] < 0:
+            print("盤面の外に置こうとしています")
+            self.select_locate(field)
         if field[self.input_num[0]][self.input_num[1]] != 0:
             print("その場所に置くことはできません")
             self.select_locate(field)
-
+            
 if __name__ == '__main__':
     test = Select_locate()
     test.select_locate()
